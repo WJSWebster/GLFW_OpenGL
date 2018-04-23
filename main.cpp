277c760ecd367c2384e9d2a13d8ec04ed50a2ba1
WJSWebster <ZBAC013@live.rhul.ac.uk>
2018-04-23 18:20:22 +0100

    (made some v minor fixes)

--- main.cpp
+++ main.cpp
@@ -27,10 +27,9 @@ int main(void)
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-//        glClear(GL_COLOR_BUFFER_BIT);
-//        glClearColor(1.0f, 0.5f, 0.2f, 1.0f); // Triangle colour
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
+        // OpenGL Legacy code (not Modern OpenGL)
         glBegin(GL_TRIANGLES);
         glVertex2f(-0.5f, -0.5f);
         glVertex2f(0.0f, 0.5f);
